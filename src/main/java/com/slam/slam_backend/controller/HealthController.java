b171d23c982f80fab81485f58e9c35fe350074a0
DESKTOP-NQ5769U\손형민 <joshua57@naver.com>
2025-09-01 13:28:25 +0900

    14분마다 깨우기

---
+++ src/main/java/com/slam/slam_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+// HealthController.java
+package com.slam.slam_backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
